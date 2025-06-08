bc4df7f74467ff1e33e4c7e0454a7c9d5dff507a
alvmiller <134068320+alvmiller@users.noreply.github.com>
2025-06-08 12:51:17 +0300

    Create sample.c

---
+++ sample.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// https://github.com/Swepz/Simple_CMake_Project
+// https://lappweb.in2p3.fr/~paubert/ASTERICS_HPC/2-2-100.html
+// https://coderefinery.github.io/cmake/03-hello-world-cmake/
+
+// mkdir build
+// (or other dir)
+// cd build
+// cmake ..
+// make
+// ./sample
+
+int main()
+{
+    printf("Hello Sample!\n");
+    return 0;
+}
